5b32a05a6dedab26eb222a01d265c45dfaba4e2f
ParsaBordbar <parsab71@gmail.com>
2024-03-13 15:43:23 +0330

    Fix missing pading and Typo bug.

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -5,11 +5,11 @@ const Hero = () => {
         <div className="flex bg-[var(--red)] justify-center items-center w-full py-10">
             <div className="flex flex-col justify-center gap-4 items-center mt-28">
                 <div>
-                    <div className="flex flex-row gap-8">
-                        <h1 className="text-5xl text-[var(--white)] lobster-regular ps-6">This is  a </h1>
+                    <div className="flex flex-row gap-5">
+                        <h1 className="text-5xl text-[var(--white)] lobster-regular ps-4">This is  a </h1>
                         <img className="pt-2" src="/src/assets/images/Ms Chalice C 1.svg" alt="" />
                     </div>
-                    <h1 className="text-5xl text-[var(--white)] lobster-regular"> Cuphead a website!</h1>
+                    <h1 className="text-5xl text-[var(--white)] lobster-regular"> Cuphead website!</h1>
                 </div>
                 <div className="px-24 flex justify-center items-center">
                     <p className="livvic-regular text-white text-center">small text here small text here
