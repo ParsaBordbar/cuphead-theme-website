c70142f568f31b5eb20dbc153538c3b09e3a9929
ParsaBordbar <parsab71@gmail.com>
2024-03-16 20:07:02 +0330

    Add Platform Component. Add styles and Structure to it. Finish Component.

--- src/components/PlatformsBox/index.tsx
+++ src/components/PlatformsBox/index.tsx
@@ -1,10 +1,17 @@
 
 const PlatformBox = () => {
     return(
-        <section className="bg-[var(--yellow)] p-6 lobster-regular flex justify-center items-center flex-col">
+        <section className="bg-[var(--yellow)] p-6 lobster-regular flex justify-center items-center flex-col gap-4">
             <h2 className="text-4xl">Available on</h2>
-            <div><img src="/src/assets/images/ps4.svg" alt="" /></div>
-            <div></div>
+            <div className="flex flex-row gap-4"> 
+                <img src="/src/assets/images/ps4.svg" alt="" /> 
+                <img src="/src/assets/images/switch.svg" alt="" />
+                <img src="/src/assets/images/gog.svg" alt="" /> 
+            </div>
+            <div className="flex flex-row gap-4">
+                <img src="/src/assets/images/xbox.svg" alt="" />
+                <img src="/src/assets/images/steam.svg" alt="" />
+            </div>
         </section>
     )
 };
