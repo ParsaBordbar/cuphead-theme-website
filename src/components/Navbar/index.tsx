e6dc529ba18df9dc9a6e76e68eb91fd3ca4ed1e4
ParsaBordbar <parsab71@gmail.com>
2025-03-17 01:28:53 +0330

    feat: deploy on github pages

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Styles from "./navbar.module.scss";
 import { motion } from "framer-motion";
 import NavElement from "../NavElement";
